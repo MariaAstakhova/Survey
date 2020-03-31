b46ea2b8a456dc4f66f0925439d6e11d358c97de
Maria Astakhova <maria@madetech.com>
2020-03-31 14:54:41 +0100

    Test a local postgres connection inside the controller

--- api/Controllers/QuestionnaireController.cs
+++ api/Controllers/QuestionnaireController.cs
@@ -20,6 +20,27 @@ namespace Survey
     [HttpGet]
     public IEnumerable<Questionnaire> ListAllProducts()
     {
+      bool boolfound = false;
+      using (NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1; Port=5432; User Id=maria; Password=me; Database=surveyApplication"))
+      {
+        conn.Open();
+
+        NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM survey", conn);
+        NpgsqlDataReader dr = cmd.ExecuteReader();
+        if (dr.Read())
+        {
+          boolfound = true;
+
+
+          Console.WriteLine("connection established");
+        }
+        if (boolfound == false)
+        {
+          Console.WriteLine("Data does not exist");
+        }
+        dr.Close();
+      }
+
       gateway = new GetQuestionnairesGateway();
       fetchQuestionnaires = new FetchQuestionnaires(gateway);
       return fetchQuestionnaires.Execute();
