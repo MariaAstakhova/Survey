a7f3f992851370daad5327ddc69e0bc77a4c4218
Maria Astakhova <maria@madetech.com>
2020-03-31 14:50:15 +0100

    add gateway unit testing, need to figure out how to mock a db object as the gateway will expect to have a Type

---
+++ api.test/Gateways/GetQuestionnairesGateway.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+using Moq;
+using System.Collections.Generic;
+
+namespace Survey
+{
+  public class GetQuestionnairesGatewayUnitTest
+  {
+    GetQuestionnairesGateway gateway;
+  
+    public class db
+    {
+      public db()
+      {
+      }
+
+      public IEnumerable<Questionnaire> request()
+      {
+        Questionnaire[] questionnaires = new Questionnaire[]
+        {
+            new Questionnaire { },
+            new Questionnaire { },
+        };
+        return questionnaires;
+      }
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public void itGetsQuestionnaires()
+    {
+      gateway = new GetQuestionnairesGateway();
+    }
+  }
+}
