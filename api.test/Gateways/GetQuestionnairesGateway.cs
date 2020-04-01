29faefbeb048ebf2fa7c25707029989444b51812
Maria Astakhova <maria@madetech.com>
2020-04-01 16:27:13 +0100

    not sure how to test gateway db connection

--- api.test/Gateways/GetQuestionnairesGateway.cs
+++ api.test/Gateways/GetQuestionnairesGateway.cs
@@ -1,28 +1,46 @@
 using NUnit.Framework;
+using Moq;
 using System.Collections.Generic;
+using Npgsql;
 
 namespace Survey
 {
   public class GetQuestionnairesGatewayUnitTest
   {
+
+    public class MockConnection
+    {
+      public bool Opened { get; set; }
+
+      public MockConnection()
+      {
+        Opened = false;
+      }
+
+      public void Open()
+      {
+        Opened = true;
+      }
+
+    }
+
+    public class MockCommand
+    {
+
+      public MockCommand(string query)
+      {
+
+      }
+
+    }
+
     GetQuestionnairesGateway gateway;
 
-    public class db
+    Questionnaire[] questionnaires = new Questionnaire[]
     {
-      public db()
-      {
-      }
-
-      public IEnumerable<Questionnaire> request()
-      {
-        Questionnaire[] questionnaires = new Questionnaire[]
-        {
             new Questionnaire { },
             new Questionnaire { },
-        };
-        return questionnaires;
-      }
-    }
+    };
 
     [SetUp]
     public void Setup()
@@ -31,9 +49,23 @@ namespace Survey
     }
 
     [Test]
-    public void itGetsQuestionnaires()
+    public void itOpensAConnection()
     {
-      // gateway = new GetQuestionnairesGateway();
+      MockConnection conn = new MockConnection();
+      NpgsqlCommand command = new NpgsqlCommand();
+      string choices = "choices array";
+      gateway = new GetQuestionnairesGateway(choices, command, conn);
+
+      // mockDb.Verify(mockDb => mockDb.Open(), Times.Once());
+      //.Setup(foo => foo.DoSomething("ping")).Returns(true);
+
+    }
+
+    [Test]
+    public void itQueriesDbForOneTopic()
+    {
+
+
     }
   }
 }
